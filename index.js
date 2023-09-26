ce5b679c1697d42c8b5494522c348b934ab15110
David <grayd500@gmail.com>
2023-09-26 16:44:32 -0500

    Update JS with MVP

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import the 'inquirer' module
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // Create an array of questions for the user
 const questions = [
@@ -8,36 +9,63 @@ const questions = [
     name: 'name',
     message: 'What is your name?',
   },
-];
 
-inquirer.prompt(questions).then((answers) => {
-  console.log(answers);
-});
+  {
+    type: 'input',
+    name: 'location',
+    message: 'What city and state are you in?',
+  },
+
+  {
+    type: 'input',
+    name: 'bio',
+    message: 'Tell us a little bit about yourself',
+  },
+
+  {
+    type: 'input',
+    name: 'linkedIn',
+    message: 'Please provide a link to your Linkedin profile.',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Please provide a link to your github profile.',
+  }
+];
 
 // Use inquirer to collect user input
 function generateHTML(answers) {
-// Create an HTML template with user input
     return `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <style>
-        // Your CSS goes here
-      </style>
-    </head>
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    /* Your CSS goes here */
+  </style>
+</head>
     <body>
       <h1>${answers.name}</h1>
+      <p>Location: ${answers.location}</p>
+      <p>Bio: ${answers.bio}</p>
+      <p>LinkedIn: <a href="${answers.linkedIn}">${answers.linkedIn}</a></p>
+      <p>GitHub: <a href="${answers.github}">${answers.github}</a></p>
       <!-- More HTML here -->
     </body>
     </html>
     `;
   }
 
+  // Use inquirer to collect user input
+inquirer.prompt(questions)
+.then((answers) => {
+  console.log(answers);
+
   const fs = require('fs');
 
-// Write the HTML template to a file
-fs.writeFile('portfolio.html', generateHTML(answers), (err) => {
-  if (err) throw err;
-  console.log('Portfolio generated!');
+ // Write the HTML template to a file
+ fs.writeFile('portfolio.html', generateHTML(answers), (err) => {
+    if (err) throw err;
+    console.log('Portfolio generated!');
+  });
 });
-
